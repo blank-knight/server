abb774369bdc7bdadbc04a8e795ba162a46083e1
storm_knight <39646095+blank-knight@users.noreply.github.com>
2023-11-25 11:40:38 +0800

    Update processpool.h

--- processpool/processpool.h
+++ processpool/processpool.h
@@ -70,16 +70,11 @@ processpool<T>::processpool( int listenfd, int _max_processes_num ):
 						   max_processes_num( _max_processes_num ), sub_processes( nullptr )
 {
 	sub_processes = new process [ max_processes_num ];
-	//这行代码用于在堆上动态分配一片内存空间，用于存储 max_processes_num 个 process 类型的对象。sub_processes 是一个指针，指向这个动态数组的首地址
 	for( int i = 0; i < max_processes_num; ++i )
 	{
 		socketpair( PF_UNIX, SOCK_STREAM, 0, sub_processes[i].pipe );//同一主机上的不同进程之间进行双向通信
 		sub_processes[i].pid = fork(); // 子进程fork后会从fork处开始执行
-		//当前进程为父进程
-		//返回两次。在父进程中，它返回子进程的PID，而在子进程中，它返回0。
 		if( sub_processes[i].pid > 0 )	// 父进程 关闭子进程方的pipe
-		//关闭管道写端（sub_processes[i].pipe[1]）：在父进程中，我们关闭子进程方的管道写端。这是因为父进程不需要向子进程写入数据，只需要从子进程读取数据。因此，关闭子进程的写端确保父进程无法向子进程写入数据，避免出现不必要的通信问题。
-		// 关闭管道读端（sub_processes[i].pipe[0]）：在子进程中，我们关闭父进程方的管道读端。这是因为子进程不需要从父进程读取数据，只需要向父进程写入数据。因此，关闭父进程的读端确保子进程无法从父进程读取数据，避免出现不必要的通信问题。
 		{
 			close( sub_processes[i].pipe[1] );
 			continue;
@@ -89,23 +84,12 @@ processpool<T>::processpool( int listenfd, int _max_processes_num ):
 			close( sub_processes[i].pipe[0] );
 			idx = i;
 			break;
-		}//在父进程中，关闭了子进程的写入端；在子进程中，关闭了父进程的读取端。
+		}
 	}
 }
 
 static int set_non_blocking( int fd )
 {
-	//int set_non_blocking(int fd): 这是函数的声明，它接收一个整数类型的参数 fd，表示要设置为非阻塞模式的文件描述符。函数返回一个整数，表示设置前的文件状态标志。
-
-	// int old_state = fcntl(fd, F_GETFL): fcntl 是一个系统调用函数，用于对文件描述符进行各种操作。在这里，使用 F_GETFL 参数来获取当前文件描述符 fd 的状态标志。这个状态标志包含了文件描述符的访问模式和一些其他标志。
-
-	// int new_state = old_state | O_NONBLOCK: 这一行使用按位或运算将 O_NONBLOCK 标志添加到 old_state 中，从而将文件描述符设置为非阻塞模式。O_NONBLOCK 是一个标志常量，表示非阻塞模式。
-
-	// fcntl(fd, F_SETFL, new_state): 使用 F_SETFL 参数来设置文件描述符 fd 的状态标志为 new_state，即将其设置为非阻塞模式。
-
-	// return old_state: 最后，函数返回设置前的文件状态标志，这样在需要的时候可以还原文件描述符的阻塞状态。
-
-	// 总结：这个函数 set_non_blocking 用于将给定的文件描述符设置为非阻塞模式。通过将 O_NONBLOCK 标志添加到文件描述符的状态标志中，可以使读写操作在没有数据可用时立即返回，而不是阻塞等待数据的到来。函数还返回设置前的文件状态标志，以备后续需要还原文件描述符阻塞状态的情况。
     int old_state = fcntl( fd, F_GETFL );//查询旧状态
     int new_state = old_state | O_NONBLOCK; // 设为非阻塞
     fcntl( fd, F_SETFL, new_state ); // 设为非阻塞
